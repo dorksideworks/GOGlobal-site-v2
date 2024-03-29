be068e7c52354bb741d0c3a99011864cfc8eb2a8
adrian <adrianraycasas@gmail.com>
2024-03-29 16:01:21 +0400

    added product description

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -6,7 +6,7 @@ function Products() {
     <div id="ProductSection" class="wrapper mx-auto relative flex flex-col py-10 pt-40">
 
         <h1 class="text-white text-5xl font-bold"> Our <span class="text-cyan-500">Products</span> </h1>
-        <p class="text-gray-500 mt-5"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ut arcu ut purus tincidunt ultrices ac at turpis. Quisque vel porttitor magna. Quisque malesuada rutrum tortor, et sodales urna varius vel.</p>
+        <p class="text-gray-500 mt-5"> Unlock your networking success through top-tier education, world-class entertainment, and cutting-edge innovations.</p>
         <div class="flex flex-col md:flex-row lg:flex-row gap-5 wrapper mx-auto mt-12">
             <a target="_blank" href="https://golearn.goglobal.network" class="group p-5 basis-1/4 flex justify-center">
                 <img class="w-40 object-contain duration-500 brightness-50 group-hover:brightness-100" src="/ASSETS/logos/go-learn.png"></img>
